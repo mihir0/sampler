ed9adc6c3fe65ca7689dc9210e27b06f0a0d5923
mihirparshy <mihir.parshy@hotmail.com>
2020-09-03 02:37:43 -0400

    multiple notes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Sampler:
     def __init__(self):
         self.recording = []
         self.samples = {} #key: String, value: loaded sample (numpy array)
+        self.chunk_size = 64 # number of samples to process/stream at once
     def load(self, path_map):
         """
             path_map (Python Dict)
@@ -26,6 +27,7 @@ class Sampler:
             nsamples = nframes * 1
             channels = wf.getnchannels()
             audio_arr = np.frombuffer(audio, dtype=np.int8)
+            print("ndim: ", audio_arr.ndim, " shape: ", audio_arr.shape)
             print("loaded ", path_map[key], " nframes=", nframes, " nsamples=", nsamples, " samplewidth=", samplewidth, " channels=", channels)
             self.samples[key] = audio_arr
         print("self.samples = ", self.samples)
@@ -39,16 +41,17 @@ class Sampler:
         end_time = time.time()
         print(end_time - start_time)
     
-    def processing_block(self, stream, loaded_wav, start_index, chunk_size):
+    def process_note_chunk(self, stream, loaded_wav, start_index, chunk_size):
         """
             Parameters:
-                stream: initialized PyAudio output stream
                 loaded_wav: numpy array initialized with audio frames
                 start_index: long data type indicating where to start playing sample
                 chunk_size: number of samples to stream in one block
             Returns:
                 end_index: long containing last sample played
+                buffer: rendered samples for the wav file
         """
+        # sum_of_notes = np.zeros(chunk_size)
         #pre-processing
         end_index = min(len(loaded_wav), start_index + chunk_size) # exclusive upper limit
         adsr = [.01 * 44100.0, 0, 0, 0, 0 , 0] # ENVELOPE: [attack time, decay time, sustain amplitude, release time]
@@ -57,15 +60,19 @@ class Sampler:
         #process buffer
         # print("length of buffer: ", len(buffer))
         for i in range(0, len(buffer)):
-            #Attack
-            if start_index < adsr[0]:
-                buffer[i] = int(round(float(buffer[i]) * attack_slope * (i + start_index)))
-        # if len(buffer) > 0:
+            pass
+            # #Attack
+            # if start_index < adsr[0]:
+            #     buffer[i] = int(round(float(buffer[i]) * attack_slope * (i + start_index)))
+            # buffer[i] = int(round(.5 * buffer[i])) #reduce volume
         # print("buffer: ", buffer)
+        # sum_of_notes = np.add(sum_of_notes, buffer)
         #stream buffer
-        stream.write(buffer)
-        self.recording.append(buffer.tolist())
-        return end_index
+        # stream.write(buffer)
+        # stream.write(sum_of_notes)
+
+        # self.recording.append(buffer.tolist())
+        return end_index, buffer
         
     def start(self):
         CHUNK = 1024
@@ -107,17 +114,33 @@ class Sampler:
             "format=", p.get_format_from_width(wf.getsampwidth()),
             ", channels=", wf.getnchannels(),
             ", rate=", wf.getframerate())
-        current_index = 0
+        # current_index = 0
+        #initialize current index
+        current_index = self.samples.copy()
+        for key in current_index:
+            current_index[key] = 0
+        chunks = self.samples.copy()
+        for key in chunks:
+            chunks[key] = np.zeros(1, dtype=np.int8)
+        print("chunks: ", chunks)
+        sum_of_chunks = np.zeros(self.chunk_size, dtype=np.int8)
         while not keyboard.is_pressed('q'):
-            # if keyboard.is_pressed('a'):
-            #     current_index = self.processing_block(stream, audio_arr, current_index, 128)
-            # else:
-            #     current_index = 0
             for key in self.samples:
                 if keyboard.is_pressed(key):
-                    current_index = self.processing_block(stream, self.samples[key], current_index, 128)
+                    current_index[key], chunks[key] = self.process_note_chunk(stream, self.samples[key], current_index[key], self.chunk_size)
                 else:
-                    current_index = 0
+                    current_index[key] = 0
+                    chunks[key] = np.zeros(1, dtype=np.int8)
+                # if chunks[key] != -1:
+                # print("chunks[key]: ", chunks[key])
+                # if len(chunks[key]) > 0:
+            for i in range(0, self.chunk_size):
+                sum_of_chunks[i] = 0
+                for key in chunks:
+                    # print("sum_of_chunks: ", sum_of_chunks, ", chunk[key]: ", chunks[key])
+                    if i < len(chunks[key]):
+                        sum_of_chunks[i] = sum_of_chunks[i] + chunks[key][i]
+            stream.write(sum_of_chunks)
         # current_index = 0
         # print("nsamples: ", nsamples)
         # print("len(audio_arr): ", len(audio_arr))
@@ -146,5 +169,5 @@ class Sampler:
 if __name__ == "__main__":
     sampler = Sampler()
     # sampler.load({"a": "note.wav", "s": "note2.wav"})
-    sampler.load({"a":"note.wav", "s":"note2.wav"})
+    sampler.load({"a":"note.wav", "s":"note2.wav", "d": "note3.wav"})
     sampler.start()
