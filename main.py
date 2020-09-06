9786900b78cf39a1ab45e2d5f50f6aae95302ae3
mihirparshy <mihir.parshy@hotmail.com>
2020-09-06 18:43:36 -0400

    volume adjusting loaded samples

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Sampler:
         self.recording = []
         self.samples = {} # key: String note name, value: loaded sample (numpy array)
         self.pos = {} # key: String note name, value integer indicating current playback position
-        self.chunk_size = 16 # number of samples to process/stream at once
+        self.chunk_size = 32 # number of samples to process/stream at once
         self.dtype = np.int16
         self.stream = None
         self.channels = 2
@@ -37,7 +37,11 @@ class Sampler:
             samplewidth = wf.getsampwidth()
             nsamples = nframes * 1
             channels = wf.getnchannels()
-            audio_arr = np.frombuffer(audio, dtype=self.dtype)
+            np_data = np.frombuffer(audio, dtype=self.dtype)
+            # reduce volume
+            audio_arr = np.zeros(np_data.size, dtype=self.dtype)
+            for i in range(np_data.size):
+                audio_arr[i] = int(round(np_data[i] * .5))
             print("ndim: ", audio_arr.ndim, " shape: ", audio_arr.shape)
             print("loaded ", path_map[key], " nframes=", nframes, " nsamples=", nsamples, " samplewidth (in bytes)=", samplewidth, " channels=", channels)
             self.samples[key] = audio_arr
@@ -48,8 +52,8 @@ class Sampler:
             self.pos[key] = 0
         # initialize output buffer
         self.output_buffer =  np.zeros(self.channels * self.chunk_size, dtype=self.dtype)
-        print("output_buffer initialized:", self.output_buffer)
-        print("output_buffer dtype:", self.output_buffer.dtype)
+        # print("output_buffer initialized:", self.output_buffer)
+        # print("output_buffer dtype:", self.output_buffer.dtype)
     def visualize(self, recording):
             recording_L = recording[::2]
             recording_R = recording[1::2]
@@ -73,12 +77,14 @@ class Sampler:
         """
         # ZERO the output buffer
         for i in range(self.output_buffer.size):
-            self.output_buffer[i] = 0
-
-        for note in notes_pressed:
-            for i in range(min(self.output_buffer.size, self.samples[note].size - self.pos[note])):
-                self.output_buffer[i] = self.samples[note][i + self.pos[note]]
-            self.pos[note] = self.pos[note] + self.output_buffer.size
+            self.output_buffer[i] = 0        
+        for note in self.samples: #NOTE: can decrease iterations by only going through notes_pressed list. Currently this implementation will detect when a "keyup" event happens...in this case, the sample position will be set back to 0
+            if note in notes_pressed:
+                for i in range(min(self.output_buffer.size, self.samples[note].size - self.pos[note])):
+                    self.output_buffer[i] = self.output_buffer[i] + self.samples[note][i + self.pos[note]]
+                self.pos[note] = self.pos[note] + self.output_buffer.size
+            else:
+                self.pos[note] = 0
         # DEBUGGING-------
         # print(self.output_buffer)
         # print(self.samples[note][:self.chunk_size * 2])
@@ -214,11 +220,11 @@ class Sampler:
         p.terminate()
         print("Finished.")
     def start(self):
-        print(sd.query_devices())
-        sd.default.samplerate = 44100
-        sd.default.channels = 2
-        sd.default.dtype = 'int16'
-        self.stream = sd.OutputStream(channels=2, samplerate=44100)
+        # print(sd.query_devices())
+        # sd.default.samplerate = 44100
+        # sd.default.channels = 2
+        # sd.default.dtype = 'int16'
+        self.stream = sd.OutputStream()
         self.stream.start()
         print("Sampler started...")
     def close(self):
@@ -255,9 +261,14 @@ class Sampler:
         self.visualize(audio_arr.tolist())
 if __name__ == "__main__":
     sampler = Sampler()
-    sampler.load({"a":"note.wav", "s":"note2.wav", "d": "note3.wav", "f": "note_R.wav"})
+    sample_map = {"a":"note.wav", "s":"note2.wav", "d": "note3.wav", "f": "note_R.wav"}
+    sampler.load(sample_map)
     sampler.start()
     while not keyboard.is_pressed('q'):
-        sampler.update(['f'])
+        keys_pressed = []
+        for key in sample_map:
+            if keyboard.is_pressed(key):
+                keys_pressed.append(key)
+        sampler.update(keys_pressed)
     sampler.close()
     # sampler.start_sounddevice()
