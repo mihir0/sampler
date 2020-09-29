a3edaff7e0af572fd6d6b478334005cb3edbb311
mihirparshy <mihir.parshy@hotmail.com>
2020-09-29 02:06:40 -0400

    cython tests

--- runtime.py
+++ runtime.py
@@ -1,4 +1,5 @@
 from sampler import Sampler
+import cython_helloworld as hw
 import time
 
 sampler = None
@@ -24,8 +25,25 @@ def time_block():
 def time_1_sec():
     pass
 
+def time_arr_fill(repetitions=1):
+    start = time.time_ns()
+    for i in range(repetitions):
+        hw.set_array(20000)
+    end = time.time_ns()
+    print(f"Time: {end-start} ns, {(end-start) / (10 ** 9)} sec")
+
+def time_arr_fill_opt(repetitions=1):
+    start = time.time_ns()
+    for i in range(repetitions):
+        hw.set_array_optimized(20000)
+    end = time.time_ns()
+    print(f"Time: {end-start} ns, {(end-start) / (10 ** 9)} sec")
+
+    
 if __name__ == "__main__":
-    setup()
+    # setup()
+    time_arr_fill(1000)
+    time_arr_fill_opt(1000)
 
 # Target benchmarks
 # current time for 64 * 2 sample block: 96858200 ns = 0.0968582
