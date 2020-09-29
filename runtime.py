448777a5308f491a3bcbb97e401a772d47ad96e4
mihirparshy <mihir.parshy@hotmail.com>
2020-09-29 19:21:24 -0400

    testing by playing all notes simultaneously

--- runtime.py
+++ runtime.py
@@ -7,7 +7,7 @@ sample_map = None
 
 def setup():
     global sampler, sample_map
-    sampler = Sampler(sample_rate=48000)
+    sampler = Sampler(sample_rate=48000, record_enabled=True)
     # sample_map = {"a":"01.wav", "s":"02.wav", "d": "03.wav", "f": "04.wav"}
     sample_map = {"a":"01.wav", "s":"02.wav", "d": "03.wav", "f": "04.wav", "g": "05.wav", "h": "06.wav", "j": "07.wav", "k": "08.wav", "l": "09.wav", ":": "10.wav", "'":"11.wav", "w":"12.wav", "e":"13.wav", "r":"14.wav", "t":"15.wav", "y":"16.wav", "u":"17.wav", "i":"18.wav", "o":"19.wav", "p":"20.wav", "[": "21.wav", "]":"22.wav"}
     sampler.load(sample_map, "samples/legopiano1/")
@@ -51,7 +51,8 @@ def time_arr_fill_opt(repetitions=1):
     
 if __name__ == "__main__":
     setup()
-    time_block(750)
+    # time_block(750)
+    # time.sleep(6)
     time_block_opt(750)
     # time_arr_fill(1000)
     # time_arr_fill_opt(1000)
